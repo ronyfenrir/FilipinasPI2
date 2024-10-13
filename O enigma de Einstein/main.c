5677b646c736df0ddc0b8ca84ae150c483c7133f
Rony Cordeiro <rony.maggot@hotmail.com>
2024-10-13 17:12:03 -0300

    alteração

--- O enigma de Einstein/main.c
+++ O enigma de Einstein/main.c
@@ -3,9 +3,11 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/keyboard.h>
-
 //  573 / 3 = 191 → largura
 // 644 / 4 = 161  → altura
+
+//  150 / 3 = 50 → largura
+// 300 / 6 = 50  → altura
 
 
 int main() {
@@ -16,15 +18,15 @@ int main() {
     al_init_image_addon();
     al_install_keyboard();
 
-    ALLEGRO_DISPLAY* display = al_create_display(1024, 576);
+    ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
     al_set_window_position(display, 200, 200);
-    al_set_window_title(display, "O enigma de Einstein!");
+    al_set_window_title(display, "O Enigma de Einstein");
 
     ALLEGRO_FONT* font = al_load_font("./font.ttf", 25, 0);
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
 
-    ALLEGRO_BITMAP* sprite = al_load_bitmap("./dragon.png");
-    ALLEGRO_BITMAP* bg = al_load_bitmap("./bg.png");
+    ALLEGRO_BITMAP* sprite = al_load_bitmap("./felipe-spriteSheet.png");
+    ALLEGRO_BITMAP* bg = al_load_bitmap("./bg1-open-door.png");
 
     ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
     al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -33,8 +35,8 @@ int main() {
     al_start_timer(timer);
 
     float frame = 0.f;
-    int pos_x = 0, pos_y = 0;
-    int current_frame_y = 161;
+    int pos_x = 0, pos_y = 670;
+    int current_frame_y = 0;
 
     while (true) {
         ALLEGRO_EVENT event;
@@ -43,32 +45,22 @@ int main() {
             break;
         }
         else if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
-            current_frame_y = 161;
-            pos_x += 20;
+            current_frame_y = 50 * 2;
+            pos_x += 10;
         }
         else if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
-            current_frame_y = 161 * 3;
-            pos_x -= 20;
+            current_frame_y = 50 * 3;
+            pos_x -= 10;
         }
-        else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) {
-            current_frame_y = 161 * 2;
-            pos_y += 20;
-        }
-        else if (event.keyboard.keycode == ALLEGRO_KEY_UP) {
-            current_frame_y = 0;
-            pos_y -= 20;
-        }
-
+        
         frame += 0.3f;
-        if (frame > 3) {
-            frame -= 3;
+        if (frame > 2) {
+            frame -= 2;
         }
 
         al_clear_to_color(al_map_rgb(255, 255, 255));
         al_draw_bitmap(bg, 0, 0, 0);
-        al_draw_text(font, al_map_rgb(0, 0, 0), 7, 7, 0, "SCORE: dragon");
-        al_draw_text(font, al_map_rgb(255, 255, 255), 5, 5, 0, "SCORE: dragon");
-        al_draw_bitmap_region(sprite, 191 * (int)frame, current_frame_y, 191, 161, pos_x, pos_y, 0);
+        al_draw_bitmap_region(sprite, 50 * (int)frame, current_frame_y, 50, 50, pos_x, pos_y, 0);
         al_flip_display();
     }
 
